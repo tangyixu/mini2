30cfc1a68b23c0a4675d71cfd15fc00bdd4572bc
tangyixu <tangyixu@grinnell.edu>
2024-10-15 22:27:54 -0500

    Attempted InteractiveCalculator

--- src/main/java/edu/grinnell/csc207/InteractiveCalculator.java
+++ src/main/java/edu/grinnell/csc207/InteractiveCalculator.java
@@ -1,5 +1,28 @@
 package edu.grinnell.csc207;
 
+
 public class InteractiveCalculator {
+  public void main (String args[]){
+     
+    while(!args[0].equals(null))
+    {
+      for(int n = 0; n < args.length; n++)
+      {
+        if (args[n].equals("/"))
+        {
+          if(Character.isDigit(args[n-1].charAt(0))){
+
+            args[n-1].toCharArray();
+
+          }
+          
+
+
+        }//if
+        
+      }
+    }//while
+  }
+  
   
 }
