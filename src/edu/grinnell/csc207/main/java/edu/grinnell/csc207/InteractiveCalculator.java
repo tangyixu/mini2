ecf29fbc77eb4f63c779ba4fb232ea05b691d40d
tangyixu <tangyixu@grinnell.edu>
2024-10-15 22:47:16 -0500

    Fix folder issues

--- src/edu/grinnell/csc207/main/java/edu/grinnell/csc207/InteractiveCalculator.java
+++ src/edu/grinnell/csc207/main/java/edu/grinnell/csc207/InteractiveCalculator.java
@@ -1,22 +1,26 @@
 package edu.grinnell.csc207;
 
+import edu.grinnell.csc207.util.BFCalculator;
+
 
 public class InteractiveCalculator {
   public void main (String args[]){
      
-    while(!args[0].equals(null))
+    while(!args[0].equals("QUIT"))
     {
-      for(int n = 0; n < args.length; n++)
+      for(int n = 1; n < args.length; n++)
       {
         if (args[n].equals("/"))
         {
           if(Character.isDigit(args[n-1].charAt(0))){
 
-            args[n-1].toCharArray();
+            BFCalculator.lastVal.add(args[n-1].toCharArray());
 
           }
-          
+          else
+          {
 
+          }
 
         }//if
         
